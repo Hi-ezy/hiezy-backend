a232e6fda8fa57ac6b823896459bf0583bce77ae
Milind <sois.milindsir@gmail.com>
2024-12-12 21:23:49 +0530

    mongodb env variable added

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const aibotrouter = require("./src/services/ai_service/ai_router")
 const interviewRouter = require("./src/services/interview_scheduling_service/interview_details_router")
 //databse connection
-mongoose.connect("mongodb://localhost:27017/hiezy");
+mongoose.connect(process.env.MONGODB_STRING);
 
 mongoose.connection.once("open", () => {
   console.log("connected with database..");
