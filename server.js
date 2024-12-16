239380e04f372ac68e85f5a26117a591450eefd2
Milind Waghmare <milind.waghmare@recity.in>
2024-12-16 22:21:56 +0530

    routes change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 
 //routing
-app.use("/", (req,res)=>{
+app.get("/", (req,res)=>{
  res.send( "Welcome to hiezy")
 })
 app.use("/app/ai",  aibotrouter)
